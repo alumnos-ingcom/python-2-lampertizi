99ca75008d2170fc20ed9d3a4f997b7888052b84
lampertizi <tizianolamperti@gmail.com>
2022-06-22 23:53:03 -0300

    embellecido para el robot

--- tests/test_5.py
+++ tests/test_5.py
@@ -9,16 +9,17 @@ se probará la función que encuentra pares de {}()[]
 """
 
 def test_encuentrapares():
+
+    
     """
     se probarán strings con letras,simbolos, y combos irregulares de {[()]}
     """
     oracion1 = "(hola)"
     oracion2 = "{{}}}"
     oracion3 = "[{!!!}]"
-    assert encuentra_pares("{}") == True, "no calcula bien un solo par"
-    assert encuentra_pares(oracion1) == True, "no calcula cuando hay letras"
-    assert encuentra_pares(oracion2) == False, "no calcula el cierre"
-    assert encuentra_pares(")]{[{") == False, "no calcula cuando hay desorden"
-    assert encuentra_pares("[}") == False, "no detecta pares"
-    assert encuentra_pares(oracion3) == True, "no detecta cuando hay simbolos"
-
+    assert encuentra_pares("{}") is True, "no calcula bien un solo par"
+    assert encuentra_pares(oracion1) is True, "no calcula cuando hay letras"
+    assert encuentra_pares(oracion2) is False, "no calcula el cierre"
+    assert encuentra_pares(")]{[{") is False, "no calcula cuando hay desorden"
+    assert encuentra_pares("[}") is False, "no detecta pares"
+    assert encuentra_pares(oracion3) is True, "no detecta cuando hay simbolos"
--- tests/test_6.py
+++ tests/test_6.py
@@ -10,6 +10,8 @@ se probará las funciones de de/codificacion en cifrado cesar
 """
 
 def test_julio():
+
+
     """
     se probarán letrás mayus, minus, y números ; tmb desencriptacion
     como tmb llaves negativas
@@ -19,14 +21,13 @@ def test_julio():
     t3 = "wxyz"
     t4 = "VamOs dE pasEo"
 
-    r1 = "gnkz ltmcn" 
-    r2 = "LSPE QYRHS" 
+    r1 = "gnkz ltmcn"
+    r2 = "LSPE QYRHS"
 
-    assert codifica_cesar(t1,-1) == "gnkz ltmcn", "no funca con llave negativa"
-    assert codifica_cesar(t2,4) == "LSPE QYRHS", "no funca con mayus"
-    assert codifica_cesar(t3,5) == "bcde", "no funca con valores limite"
-    assert codifica_cesar(t4,14) == "JoaCg rS dogSc", "no funca con mayus + valores limite"
+    assert codifica_cesar(t1, -1) == "gnkz ltmcn", "no funca con llave negativa"
+    assert codifica_cesar(t2, 4) == "LSPE QYRHS", "no funca con mayus"
+    assert codifica_cesar(t3, 5) == "bcde", "no funca con valores limite"
+    assert codifica_cesar(t4, 14) == "JoaCg rS dogSc", "no funca con mayus + valores limite"
 
-    assert decodifica_cesar(r2,4) == "HOLA MUNDO", "no funciona el decoder"
-    assert decodifica_cesar(r1,-1) == "hola mundo", "no funciona el decoder"
-
+    assert decodifica_cesar(r2, 4) == "HOLA MUNDO", "no funciona el decoder"
+    assert decodifica_cesar(r1, -1) == "hola mundo", "no funciona el decoder"
